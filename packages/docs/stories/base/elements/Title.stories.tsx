9d0dd84475d779607496d2f454115eb526ed3d77
Bastiaan Stroosnijder <bas@strootje.com>
2020-09-09 20:47:37 +0200

    fixed error

--- packages/docs/stories/base/elements/Title.stories.tsx
+++ packages/docs/stories/base/elements/Title.stories.tsx
@@ -19,8 +19,8 @@ export const Basic = (props: TitleProps) => (
 	<Fragment>
 		{Array.apply(null, Array(5)).map((_, i) => (
 			<Fragment>
-				<Title size={i + 1} {...props}>Title {i + 1}{i === 2 && ' (default size)'}</Title>
-				<SubTitle size={i + 3}  {...props}>Subtitle {i + 3}{i === 2 && ' (default size)'}</SubTitle>
+				<Title size={(i + 1) as (1 | 2 | 3 | 4 | 5 | 6)} {...props}>Title {i + 1}{i === 2 && ' (default size)'}</Title>
+				<SubTitle size={(i + 3) as (3 | 4 | 5 | 6 | 7)}  {...props}>Subtitle {i + 3}{i === 2 && ' (default size)'}</SubTitle>
 			</Fragment>
 		))}
 	</Fragment>
