1e0296ca14fb6e72b9519e3cb411bbbe8e9942d5
Bastiaan Stroosnijder <bas@strootje.com>
2020-09-09 21:49:54 +0200

    lets go

--- packages/docs/stories/base/components/Breadcrumb.stories.tsx
+++ packages/docs/stories/base/components/Breadcrumb.stories.tsx
@@ -1,5 +1,5 @@
 import { action } from '@storybook/addon-actions';
-import { Box, Breadcrumb, BreadcrumbItem, BreadcrumbProps } from 'chichi-base';
+import { Box, Breadcrumb, BreadcrumbItem, BreadcrumbProps, Icon } from 'chichi-base';
 import { h } from 'preact';
 
 export default {
@@ -45,6 +45,19 @@ export const Basic = (props: BreadcrumbProps) => (
 			<Breadcrumb.Item onClick={action('clicked on item 2')}>Documentation</Breadcrumb.Item>
 			<Breadcrumb.Item onClick={action('clicked on item 3')}>Components</Breadcrumb.Item>
 			<Breadcrumb.Item active>Breadcrumb</Breadcrumb.Item>
+		</Breadcrumb>
+	</Box>
+);
+
+
+
+export const WithIcons = (props: BreadcrumbProps) => (
+	<Box>
+		<Breadcrumb {...props}>
+			<Breadcrumb.Item onClick={action('clicked on item 1')}><Icon size='small' icon='fas fa-home' /><span>Bulma</span></Breadcrumb.Item>
+			<Breadcrumb.Item onClick={action('clicked on item 2')}><Icon size='small' icon='fas fa-book' /><span>Documentation</span></Breadcrumb.Item>
+			<Breadcrumb.Item onClick={action('clicked on item 3')}><Icon size='small' icon='fas fa-puzzle-piece' /><span>Components</span></Breadcrumb.Item>
+			<Breadcrumb.Item active><Icon size='small' icon='fas fa-thumbs-up' /><span>Breadcrumb</span></Breadcrumb.Item>
 		</Breadcrumb>
 	</Box>
 );
--- packages/docs/stories/base/components/Card.stories.tsx
+++ packages/docs/stories/base/components/Card.stories.tsx
@@ -1,5 +1,5 @@
-import { Card, Content } from 'chichi-base';
-import { h } from 'preact';
+import { Card, Content, Notification, SubTitle, Title } from 'chichi-base';
+import { Fragment, h } from 'preact';
 
 export default {
 	title: 'Base/Components/Card',
@@ -7,33 +7,74 @@ export default {
 };
 
 export const Basic = () => (
-	<Card>
-		<Card.Image>
-			<figure class="image is-4by3">
-				<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
-			</figure>
-		</Card.Image>
-
-		<Card.Content>
-			<div class="media">
-				<div class="media-left">
-					<figure class="image is-48x48">
-						<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+	<Fragment>
+		<Notification>
+			<Card>
+				<Card.Image>
+					<figure class="image is-4by3">
+						<img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
 					</figure>
-				</div>
-				<div class="media-content">
-					<p class="title is-4">John Smith</p>
-					<p class="subtitle is-6">@johnsmith</p>
-				</div>
-			</div>
+				</Card.Image>
 
-			<Content>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				<Card.Content>
+					<div class="media">
+						<div class="media-left">
+							<figure class="image is-48x48">
+								<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+							</figure>
+						</div>
+						<div class="media-content">
+							<p class="title is-4">John Smith</p>
+							<p class="subtitle is-6">@johnsmith</p>
+						</div>
+					</div>
+
+					<Content>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 				Phasellus nec iaculis mauris. <a>@bulmaio</a>.
 				<a href="#">#css</a> <a href="#">#responsive</a>
-				<br />
-				<time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-			</Content>
-		</Card.Content>
-	</Card>
+						<br />
+						<time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+					</Content>
+				</Card.Content>
+			</Card>
+		</Notification>
+
+		<Notification>
+			<Card>
+				<Card.Header>
+					<Card.Title>Component</Card.Title>
+					<Card.Icon icon='fas fa-angle-down' />
+				</Card.Header>
+
+				<Card.Content>
+					<Content>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris. <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
+						<br />
+						<time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+					</Content>
+				</Card.Content>
+
+				<Card.Footer>
+					<Card.FooterItem>Save</Card.FooterItem>
+					<Card.FooterItem>Edit</Card.FooterItem>
+					<Card.FooterItem>Delete</Card.FooterItem>
+				</Card.Footer>
+			</Card>
+		</Notification>
+
+		<Notification>
+			<Card>
+				<Card.Content>
+					<Title>“There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”</Title>
+					<SubTitle>Jeff Atwood</SubTitle>
+				</Card.Content>
+
+				<Card.Footer>
+					<Card.FooterItem>View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a></Card.FooterItem>
+					<Card.FooterItem>Share on <a href="#">Facebook</a></Card.FooterItem>
+				</Card.Footer>
+			</Card>
+		</Notification>
+	</Fragment>
 );
